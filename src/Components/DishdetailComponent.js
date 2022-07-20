fcb4dbd8657e1269b8dae77ee3f8de06e80cc7c3
borehelu <borehelu@gmail.com>
2022-07-20 13:48:06 +0300

    Component Assignment

---
+++ src/Components/DishdetailComponent.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import { Card, CardImg, CardText, CardBody,
+    CardTitle } from 'reactstrap';
+
+export class DishdetailComponent extends Component {
+
+    renderDish(dish){
+        return (
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        )
+    }
+
+    renderComments(comments){
+        let commentJsx = null;
+        if(comments != null ){
+             commentJsx = comments.map(comment => {
+                console.log(comment.author);
+                return (
+                    <>
+                    <li className="list-unstyled">{comment.comment}</li>
+                    <li className="list-unstyled">--{comment.author}, {comment.date}</li>
+                    </>
+                );
+            })
+        }else{
+            commentJsx = <div></div>
+
+        }
+    
+
+        return commentJsx;
+        
+    }
+   
+  render() {
+
+    return (
+        
+            
+                (this.props.selectedDish != null)?
+                (
+                    <div className="row">
+                    <div  className="col-12 col-md-5 m-1">
+                        {this.renderDish(this.props.selectedDish)}
+                    </div> 
+                     <div  className="col-12 col-md-5 m-1">
+                        <h4>Comments</h4>
+                        {this.renderComments(this.props.selectedDish.comments)}
+                     </div> 
+                     </div>
+                ):
+                (
+                <div></div>
+                )
+
+            
+        
+
+     
+      
+     
+    );
+  }
+}
+
+export default DishdetailComponent
--- src/Components/MenuComponent.js
+++ src/Components/MenuComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+
+import { DishdetailComponent } from './DishdetailComponent'
 
 class MenuComponent extends Component {
 
@@ -16,22 +17,6 @@ class MenuComponent extends Component {
         this.setState({ selectedDish: dish});
     }
 
-    renderDish(dish) {
-        if (dish != null)
-            return(
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else
-            return(
-                <div></div>
-            );
-    }
 
     render() {
         const menu = this.props.dishes.map((dish) => {
@@ -53,11 +38,8 @@ class MenuComponent extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
-                  <div  className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.state.selectedDish)}
-                  </div>
-                </div>
+                <DishdetailComponent selectedDish={this.state.selectedDish}/>
+               
             </div>
         );
     }
